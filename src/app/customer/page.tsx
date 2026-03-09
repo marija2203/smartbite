6d33bd5c9c3d8826c2c2b5fb67fbaccfc3657e4e
Marija Vlaisavljević <TVOJ_GITHUB_EMAIL>
2026-03-09 20:55:52 +0100

    Move use client directive to top of customer page

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useMemo, useState } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
 import SBButton from "../components/SBButton"
-"use client";
+
 
 import { Suspense } from "react";
 
